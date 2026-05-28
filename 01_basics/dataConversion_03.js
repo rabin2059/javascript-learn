97cae98b3b9bd3ffdf954aa061f206accaf4ac4a
Rabin Rai <137150382+rabin2059@users.noreply.github.com>
2026-05-28 03:09:13 +0000

    conversion of datatype

---
+++ 01_basics/dataConversion_03.js
@@ -0,0 +1,26 @@
+let score = null
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "23" => 23
+// "23aff" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 6
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "asdf" => true
+
+let someNumber = 32
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
