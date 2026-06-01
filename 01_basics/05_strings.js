7dce4a1fee56690f60a46259a4db87b2ea5bd663
rabin2059 <rai2059rabin@gmail.com>
2026-06-01 23:21:01 +0545

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "Rabin Rai"
+const repoCount = 10
+
+// console.log(name + repoCount + " Value");
+
+console.log(`My name is ${name} and I have ${repoCount} repositories`);
+
+const gameName = new String("Chess-Master")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("e"));
+
+const newString = gameName.substring(0, 5)
+console.log(newString);
